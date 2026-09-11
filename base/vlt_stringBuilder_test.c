aa46ff2211f494c1900142ca2f19d84d22adf439
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2026-09-11 07:24:17 +0500

    Add tests for the remaining `SStringBuilder` funcs

--- base/vlt_stringBuilder_test.c
+++ base/vlt_stringBuilder_test.c
@@ -1,9 +1,14 @@
 #include "vlt_stringBuilder.h"
 #include "vctest.h"
+#include "vlt_string.h"
+#include "base.h"
+
+#include <string.h>
 
 TEST(whenNewStringBuilder_thenReturnPointerToNewStringBuilderObjWithCorrectProperties,
-     "When new_stringBuilder, then a non-NULL pointer to a new SStringBuilder object"
-     " with correctly initialized fields/properties must be returned")
+     "When new_stringBuilder, then a non-NULL pointer to a new"
+     " SStringBuilder object with correctly initialized"
+     " fields/properties must be returned")
 {
     // GIVEN
     // WHEN
@@ -18,8 +23,9 @@ TEST(whenNewStringBuilder_thenReturnPointerToNewStringBuilderObjWithCorrectPrope
 }
 
 TEST(whenAppendChar_thenAppendCharToGivenStringBuilderAndUpdateProperly,
-     "When sb_appendChar, then append a given char to a given SStringBuilder obj"
-     " buffer and update the SStringBuilder object properly")
+     "When sb_appendChar, then append a given char to a given"
+     " SStringBuilder obj buffer and update the SStringBuilder"
+     " object properly")
 {
     // GIVEN
     SStringBuilder* sb = new_stringBuilder();
@@ -46,4 +52,115 @@ TEST(whenAppendChar_thenAppendCharToGivenStringBuilderAndUpdateProperly,
 
     // CLEAN-UP
     del_stringBuilder(sb);
+}
+
+TEST(whenAppendString_thenAppendStringToGivenStringBuilderAndUpdateProperly,
+     "When sb_appendString, then append a given String bytes to a given"
+     " SStringBuilder obj buffer and update the SStringBuilder object properly")
+{
+    U64 inputsSize = 5;
+    String* inputs[] = {
+        new_stringFromLit("Hello"),
+        new_stringFromLit("Hello World"),
+        new_stringFromLit("908asg908a;kla"),
+        new_stringFromLit("0'ldsgqkh-0}kqk--a9env;ks;"),
+        new_stringFromLit(""),
+    };
+
+    for (U64 i = 0; i < inputsSize; i++)
+    {
+        // GIVEN
+        SStringBuilder* sb = new_stringBuilder();
+        VCTEST_ASSERT_TRUE(sb != NULL);
+        VCTEST_ASSERT_TRUE(sb->buf != NULL);
+        String* str = inputs[i];
+
+        // WHEN
+        sb_appendString(sb, str);
+
+        // THEN
+        VCTEST_ASSERT_TRUE(sb->len == str->len);
+        VCTEST_ASSERT_TRUE(bytesEqual(sb->buf, str->bytes, str->len));
+
+        // CLEAN-UP
+        del_stringBuilder(sb);
+    }
+
+    // CLEAN-UP
+    for (U64 i = 0; i < inputsSize; i++)
+        del_string(inputs[i]);
+}
+
+TEST(whenAppendStrLit_thenAppendStrLitToGivenStringBuilderAndUpdateProperly,
+     "When sb_appendStrLit, then append a given string literal bytes to a"
+     " given SStringBuilder obj buffer and update the SStringBuilder object"
+     " properly")
+{
+    U64 inputsSize = 5;
+    const char* const inputs[] = {
+        "Hello",
+        "Hello World",
+        "908asg908a;kla",
+        "0'ldsgqkh-0}kqk--a9env;ks;",
+        "",
+    };
+
+    for (U64 i = 0; i < inputsSize; i++)
+    {
+        // GIVEN
+        SStringBuilder* sb = new_stringBuilder();
+        VCTEST_ASSERT_TRUE(sb != NULL);
+        VCTEST_ASSERT_TRUE(sb->buf != NULL);
+        const char* const strLit = inputs[i];
+        const U64 strLitLen = strlen(strLit);
+
+        // WHEN
+        sb_appendStrLit(sb, strLit);
+
+        // THEN
+        VCTEST_ASSERT_TRUE(sb->len == strLitLen);
+        VCTEST_ASSERT_TRUE(bytesEqual(sb->buf, (const U8* const) strLit, strLitLen));
+
+        // CLEAN-UP
+        del_stringBuilder(sb);
+    }
+}
+
+TEST(whenToString_thenReturnCorrectString,
+     "When sb_ToString, then return a pointer to a String obj that"
+     " contains correct bytes")
+{
+    U64 inputsSize = 5;
+    String* inputs[] = {
+        new_stringFromLit("Hello"),
+        new_stringFromLit("Hello World"),
+        new_stringFromLit("908asg908a;kla"),
+        new_stringFromLit("0'ldsgqkh-0}kqk--a9env;ks;"),
+        new_stringFromLit(""),
+    };
+
+    for (U64 i = 0; i < inputsSize; i++)
+    {
+        // GIVEN
+        SStringBuilder* sb = new_stringBuilder();
+        VCTEST_ASSERT_TRUE(sb != NULL);
+        VCTEST_ASSERT_TRUE(sb->buf != NULL);
+        String* str = inputs[i];
+        sb_appendString(sb, str);
+
+        // WHEN
+        String* sbStr = sb_ToString(sb);
+
+        // THEN
+        VCTEST_ASSERT_TRUE(sbStr->len == str->len);
+        VCTEST_ASSERT_TRUE(bytesEqual(sbStr->bytes, str->bytes, str->len));
+
+        // CLEAN-UP
+        del_stringBuilder(sb);
+        del_string(sbStr);
+    }
+
+    // CLEAN-UP
+    for (U64 i = 0; i < inputsSize; i++)
+        del_string(inputs[i]);
 }
