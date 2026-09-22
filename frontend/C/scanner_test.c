cfe7e49872f659127c6cbf10ac048385182dbc9a
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2026-09-22 16:13:27 +0500

    Add decimal scan test, rm too large decimals from the test input file

--- frontend/C/scanner_test.c
+++ frontend/C/scanner_test.c
@@ -377,3 +377,99 @@ TEST(testScanOperators,
     del_string(testFileDirPath);
     del_string(filepath);
 }
+
+TEST(testScanDecimalLiterals,
+     "When scan a sequence of decimal literals in a file, then"
+     " nextTok should scan correct tokens")
+{
+    const U64 tokensSize = 26;
+    // Tokens and their order MUST match the tokens and the
+    // order they appear in inside the test input file
+    const U64 intValues[26] = {
+        0,
+        1,
+        2,
+        9,
+        10,
+        42,
+        99,
+        100,
+        123,
+        255,
+        256,
+        512,
+        999,
+        1000,
+        12345,
+        123456,
+        999999,
+        1000000,
+        2147483647,
+        2147483648,
+        4294967295,
+        4294967296,
+        9223372036854775,
+        1844674407370955
+    };
+
+    // GIVEN
+    SStringBuilder* sb = new_stringBuilder();
+    VCTEST_ASSERT_TRUE(sb != NULL);
+    String* testFilepath = new_stringFromLit(__FILE__);
+    VCTEST_ASSERT_TRUE(testFilepath != NULL);
+    String* testFileDirPath = dirName(testFilepath);
+    VCTEST_ASSERT_TRUE(testFileDirPath != NULL);
+    sb_appendString(sb, testFileDirPath);
+    sb_appendStrLit(sb, "tests/scanner/operators.c1");
+    String* filepath = sb_toString(sb);
+    VCTEST_ASSERT_TRUE(filepath != NULL);
+
+    SScanner* s = new_scanner(filepath);
+    VCTEST_ASSERT_TRUE(s != NULL);
+    VCTEST_ASSERT_TRUE(s->src->file != NULL);
+    VCTEST_ASSERT_TRUE(stringEqual(s->filepath, filepath));
+    VCTEST_ASSERT_TRUE(s->src->bufEnd > 0);
+
+    // Test no token has been scanned yet
+    VCTEST_ASSERT_TRUE(s->src->next == 0);
+    VCTEST_ASSERT_TRUE(s->lnOffs == 0);
+    VCTEST_ASSERT_TRUE(s->colOffs == 0);
+    VCTEST_ASSERT_TRUE(s->tok.base.ln == 0);
+    VCTEST_ASSERT_TRUE(s->tok.base.col == 0);
+    VCTEST_ASSERT_TRUE(s->tok.base.type == ETOKEN_NO_VALUE);
+
+    U64 tokLn = 1;
+    for (U64 i = 0; i < tokensSize; i++)
+    {
+        // WHEN
+        EToken tok = nextTok(s);
+
+        // THEN
+        VCTEST_ASSERT_TRUE(s->tok.base.type == tok);
+        VCTEST_ASSERT_TRUE(s->tok.base.type == ETOKEN_INT_LIT);
+        VCTEST_ASSERT_TRUE(s->tok.base.col == 1);
+        VCTEST_ASSERT_TRUE(s->tok.base.ln == tokLn);
+        VCTEST_ASSERT_TRUE(s->tok.number.val == intValues[i]);
+        tokLn++;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // Test the EOF token at the end
+    ///////////////////////////////////////////////////////////////////
+    // WHEN
+    EToken tok = nextTok(s);
+
+    // THEN
+    VCTEST_ASSERT_TRUE(s->tok.base.type == tok);
+    VCTEST_ASSERT_TRUE(s->tok.base.type == ETOKEN_EOF);
+    VCTEST_ASSERT_TRUE(s->tok.base.col == 1);
+    VCTEST_ASSERT_TRUE(s->tok.base.ln == tokLn);
+    VCTEST_ASSERT_TRUE(s->tok.number.val == intValues[tokensSize - 1]);
+
+    // CLEAN-UP
+    del_scanner(s);
+    del_stringBuilder(sb);
+    del_string(testFilepath);
+    del_string(testFileDirPath);
+    del_string(filepath);
+}
